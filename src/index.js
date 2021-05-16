40afb0ca4e888f4b076d2711d16d4b1b7f04127c
Carlos Sepulveda <carlos.sepulvedahe@gmail.com>
2021-05-15 23:42:09 -0400

    c12

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
-// const element = document.createElement('h1');
-// element.innerText = 'Hello, Platzi Badges!';
+import React from "react";
+import ReactDOM from "react-dom";
+import Badge from "./components/Badge";
+import "bootstrap/dist/css/bootstrap.css";
+import "./global.css";
 
-// const container = document.getElementById('app');
+const container = document.getElementById("app");
 
-// container.appendChild(element);
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const element = <h1>Hello, Platzi Badges!</h1>;
-
-const container = document.getElementById('app');
-
-// ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+ReactDOM.render(
+  <Badge
+    firstName="Carlos"
+    lastName="Sepúlveda"
+    jobTitle="Software Engineer"
+    twitter="CarlosSH"
+    avatarUrl="https://www.gravatar.com/avatar/3250229f150e1d56c72c7962f33daca7"
+  />,
+  container
+);
